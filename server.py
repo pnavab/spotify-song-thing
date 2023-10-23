576f03a82d64007db4dc7cda78a4caffc21a841b
pnavab <114110926+pnavab@users.noreply.github.com>
2023-10-23 13:16:28 -0700

    Fix get current playing song

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import json
 import uvicorn
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import spotipy.util
 
 import sqlite_helpers
 
@@ -13,50 +14,17 @@ keys = json.load(file)
 client_id = keys["CLIENT_ID"]
 client_secret = keys["CLIENT_SECRET"]
 scope = "user-read-currently-playing"
+username = "cuber322"
 
-# client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri="http://localhost", scope="user-read-currently-playing"))
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri="http://localhost", scope=scope))
-
-file = open(".cache")
-token_info = json.load(file)
-token = token_info["access_token"]
-
-sp = spotipy.Spotify(auth=token)
-
-current_playback = sp.current_playback()
-
-# Check if the user is currently playing a track
-if current_playback is not None and 'item' in current_playback:
-    track = current_playback['item']
-    track_name = track['name']
-    artist_name = track['artists'][0]['name']
-    print(f"Currently playing: {track_name} by {artist_name}")
-else:
-    print("No track is currently playing.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("--------------------------")
+result = sp.current_user_playing_track()
+print(result)
+print(result['item']['name'])
+artist = result['item']['artists'][0]['name']
+print(f"by {artist}")
+print("--------------------------")
 
 
 DATABASE_FILE_PATH = "songdatabase.db"
@@ -64,5 +32,5 @@ sqlite_helpers.maybe_create_table(DATABASE_FILE_PATH)
 
 @app.get("/get_current_song")
 async def get_current_song(request: Request):
-    current_song = sp.current_playback()
+    current_song = sp.current_user_playing_track()
     print(current_song['item']['name'])
